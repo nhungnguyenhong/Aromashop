9cd5819492759aee5643238a0ba620d63628ce44
nhungnguyenhong <hongnhungnguyen4092@gmail.com>
2020-04-03 16:56:51 +0700

    Create Project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { ProductProvider } from "./context";
+document.body.style.backgroundColor = "#EEEDE7";
+
+ReactDOM.render(
+  <ProductProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductProvider>,
+  document.getElementById("root")
+);
+
